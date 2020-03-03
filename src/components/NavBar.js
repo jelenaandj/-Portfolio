fc6842fa44849f85d6b2e400901f4f5fec6cdbe5
jelenaandj <jelena.andjelkovic453@gmail.com>
2020-03-03 12:02:34 +0100

    nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function NavBar() {
     return (
         <div>
-            
+            <ul>
+            <li>
+                <a href="#education">About me</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">My Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
         </div>
     )
 }
