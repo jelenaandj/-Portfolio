9578949c0f3d275484af0585424a58fb2aa85ddb
jelenaandj <jelena.andjelkovic453@gmail.com>
2020-03-04 17:34:05 +0100

    fixed nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import AboutMe from './AboutMe'
 import Skillls from './Skillls'
 import {Link} from 'react-router-dom'
@@ -7,28 +7,38 @@ import {Link} from 'react-router-dom'
 
 export default function NavBar() {
 
+    const [style1,setStyle1]=useState({color:'blue'})
+    const [style2,setStyle2]=useState({color:'blue'})
+    const [style3,setStyle3]=useState({color:'blue'})
+    const [style4,setStyle4]=useState({color:'blue'})
+    
 
     const activePg=window.location.pathname
-    console.log(activePg)
 
+    const home='/'
+    const skills='/skills'
+    const myprojects='/myprojects'
+    const contact='/contact'
+
+    // console.log(activePg)
+    
     const styleA={
         color:'yellow'
     }
-    const styleB={
+    const style={
         color:'blue'
     }
 
-    // useEffect(()=>{
-
-    //     activePg === '/' ? styleA:styleB
-    // })
+    useEffect(()=>{
+        activePg === home? setStyle1(styleA) : activePg === skills? setStyle2(styleA) : activePg === myprojects? setStyle3(styleA) : activePg === contact? setStyle4(styleA) : setStyle4(style)
+    },activePg)
 
     return (
         <div>
-            <Link to='/'><button className='homeBtn' style={activePg === '/' ? styleA:styleB} >Home</button></Link>
-            <Link to='skills'><button className='skillsBtn'style={activePg === '/skills' ? styleA:styleB}>Skillls</button></Link>
-            <Link to='myprojects'> <button className='projBtn'>My Projects</button></Link>
-            <Link to='contact'><button className='ctcBtn'>Contact</button></Link> 
+            <Link to='/'><button className='homeBtn' style={style1} >Home</button></Link>
+            <Link to='/skills'><button className='skillsBtn'style={style2}>Skillls</button></Link>
+            <Link to='/myprojects'> <button className='projBtn'style={style3}>My Projects</button></Link>
+            <Link to='/contact'><button className='ctcBtn'style={style4}>Contact</button></Link> 
         </div>
     )
 }
